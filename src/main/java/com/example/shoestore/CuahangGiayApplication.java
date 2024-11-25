97591a2180258dd8a6bd602e83cee7903e6fad35
chichuong <lychichuong@gmail.com>
2024-11-25 19:56:43 +0700

    thu

---
+++ src/main/java/com/example/shoestore/CuahangGiayApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuahangGiayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuahangGiayApplication.class, args);
+	}
+
+}
